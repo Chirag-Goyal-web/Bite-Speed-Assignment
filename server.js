10a53a4fba9e921183b7178e59797abf768d77ef
Chirag Goyal <cgoyal506@gmail.com>
2023-07-03 14:10:44 +0530

    Utility functions

--- server.js
+++ server.js
@@ -15,8 +15,28 @@ const pool = new pq.Pool({
 
 const query = (text, params) => pool.query(text, params);
 
-
 app.post("/identify", async (req,res)=>{
     const email = req.body.email;
     const phoneNumber = req.body.phoneNumber;
 });
+
+
+async function getPrimaryIdUsingPhoneNumber(phoneNumber){
+    const contactWithSamePhoneNumber = await query('SELECT * FROM "Contact" where "phoneNumber"=$1 limit 1', [phoneNumber]);
+    return contactWithSamePhoneNumber.rows[0].linkPrecedence == "primary" ?  contactWithSamePhoneNumber.rows[0].id : contactWithSamePhoneNumber.rows[0].linkedId;
+}
+
+async function getPrimaryIdUsingEmail(email){
+    const contactWithSameEmail = await query('SELECT * FROM "Contact" where email=$1 limit 1', [email]);
+    return contactWithSameEmail.rows[0].linkPrecedence == "primary" ?  contactWithSameEmail.rows[0].id : contactWithSameEmail.rows[0].linkedId;
+}
+
+async function checkIfEmailExist(email){
+    const result = await query('SELECT * from "Contact" where email= $1',[email]);
+    return result.rows.length>0;
+}
+
+async function checkIfPhoneNumberExist(phoneNumber){
+    const result = await query('SELECT * from "Contact" where "phoneNumber"= $1',[phoneNumber]);   
+    return result.rows.length>0;
+}
